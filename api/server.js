4da4217f6c708e8d234a7038312593375ab7b824
Ryan Oberly <ryanjoberly@gmail.com>
2023-08-26 17:07:32 -0600

    completed all tests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,105 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Data = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json())
+
+server.get('/hello-world', (req, res) => {
+    res.status(200).json({message: 'Hello World!!!'})
+})
+
+server.get('/api/users', async (req, res) => {
+    Data.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting user information',
+                err: err.message
+            })
+        })
+})
+server.get('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    Data.findById(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({
+                    message: `user with id ${id} does not exist`
+                })
+            } else {
+                res.json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'does not exist'
+            })
+        })
+})
+
+
+server.post('/api/users', async (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'provide name and bio'
+        })
+    } else {
+        Data.insert(user)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(400).json({
+                    message: 'could not create new user'
+                })
+            })
+    }
+})
+server.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    Data.remove(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({
+                    message: 'does not exist',
+                })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+})
+server.put('/api/users/:id', async (req , res) => {
+    const { id } = req.params;
+    const updatedUser = req.body
+    if(!updatedUser.name || !updatedUser.bio) {
+        res.status(400).json({message: 'provide name and bio'})
+    } else {
+        Data.update(id, updatedUser)
+            .then(user => {
+                if(!user) {
+                    res.status(404).json({message: 'does not exist'})
+                } else {
+                    const newUser = {
+                        bio: updatedUser.bio,
+                        id: id,
+                        name: updatedUser.name,
+                    }
+                    console.log(newUser)
+                    res.status(200).json(newUser)
+                }
+                
+            })
+        
+    }
+})
+
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
